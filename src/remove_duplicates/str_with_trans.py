c535677894fe63c428586557e46eff06f94f0323
octaprog7 <goctaprog@gmail.com>
2023-03-11 13:36:43 +0500

    Множественные изменения для интернационализации. Все строки хранятся в CSV файле.

--- src/remove_duplicates/str_with_trans.py
+++ src/remove_duplicates/str_with_trans.py
@@ -4,6 +4,7 @@ import locale
 import pathlib
 import remove_duplicates.internationalization as my_int
 
+default_encoding = "utf-8"
 # чтобы активировать пользовательскую locale! Для форматирования даты и времени!
 # locale.setlocale(locale.LC_ALL, '')
 # текущий язык локали
@@ -15,31 +16,26 @@ _I = my_int.CSVProvider(f"{src_folder}{os.path.sep}translated.csv", "strID", cur
 
 # переводимые на другие языки строки
 
-strAccessError = "Access is denied! Folder:"
-strProcessInfo = "Folder {start} processed. Found {retval} copies!"
+strAccessError = _I("strAccessError")
+strProcessInfo = _I("strProcessInfo")
 
-strDescription = """Utility to recursive search and move/delete duplicate files 
-                    of the same size and context in specified folder."""
-strEpilog = """If the storage folder is not specified, 
-                then duplicate files will be deleted!
-                If the number of command line parameters is zero, 
-                then the search folder = current folder."""
+strDescription = _I("strDescription")
+strEpilog = _I("strEpilog")
 
-strRecursiveSearchBegin = "The folder with which the recursive search begins."
-strFolderForStor = "Folder for storing duplicate files."
-strLogFileName = "Log file name."
-strFileNamePattern = "File name pattern. Only files matching the pattern are processed! " \
-                     "Provides support for Unix shell-style wildcards."
-strNotRecursively = "If this parameter is set, then recursive search is disabled!"
-strInvalidSearchFolder = "Invalid path to search folder: {search_folder}. Exit!"
-strInvalidStorageFolder = "Invalid path to storage folder: {stor_folder}. Exit!"
+strRecursiveSearchBegin = _I("strRecursiveSearchBegin")
+strFolderForStor = _I("strFolderForStor")
+strLogFileName = _I("strLogFileName")
+strFileNamePattern = _I("strFileNamePattern")
+strNotRecursively = _I("strNotRecursively")
+strInvalidSearchFolder = _I("strInvalidSearchFolder")
+strInvalidStorageFolder = _I("strInvalidStorageFolder")
 
-strSearchForDupFilesInFolder = "Search for duplicate files in the folder: {folder_name}"
-strPatternFileName = "Pattern file name: {pattern}"
-strLogFileName_1 = "Log file name: {log}"
-strStorFolder = "Storage folder: {storage}"
-strRecursiveSearchDisabled = "Recursive search disabled!"
-strActionDel = "deleted"
-strActionMov = "moved"
-strTotalFound = "Total found {total} copies of files."
-strLastInfo = "{cnt} copies of files have been {action}."
+strSearchForDupFilesInFolder = _I("strSearchForDupFilesInFolder")
+strPatternFileName = _I("strPatternFileName")
+strLogFileName_1 = _I("strLogFileName_1")
+strStorFolder = _I("strStorFolder")
+strRecursiveSearchDisabled = _I("strRecursiveSearchDisabled")
+strActionDel = _I("strActionDel")    # файл удален!
+strActionMov = _I("strActionMov")      # файл перемещен!
+strTotalFound = _I("strTotalFound")
+strLastInfo = _I("strLastInfo")
